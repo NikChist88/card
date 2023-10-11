a60ac06bca169c7846d480795c4c117805247abb
NikChist88 <NikChistyakov88@yandex.ru>
2023-10-12 00:28:04 +0300

    Add GlobalStyles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *:before,
+    *:after {
+        padding: 0px;
+        margin: 0px;
+        border: 0px;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: #f5f5f5;
+    }
+`
